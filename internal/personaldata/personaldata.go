e8672ac62c0b44c2269a6fab9282abd3ed7f50d1
Max Dukov <dukov@arrival.com>
2025-12-14 19:20:15 +0300

    очепятка

--- internal/personaldata/personaldata.go
+++ internal/personaldata/personaldata.go
@@ -9,5 +9,5 @@ type Personal struct {
 }
 
 func (p Personal) Print() {
-	fmt.Printf("Имя: %s\nВес: %f.2\nРост: %f.2\n", p.Name, p.Weight, p.Height)
+	fmt.Printf("Имя: %s\nВес: %.2f\nРост: %.2f\n", p.Name, p.Weight, p.Height)
 }
