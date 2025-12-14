f1cc1c0060fd0c194fa09a2638979ad42f7f23ac
Max Dukov <dukov@arrival.com>
2025-12-14 19:14:23 +0300

    а теперь косяк с типами тренировок

--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -43,14 +43,20 @@ func (t *Training) Parse(datastring string) (err error) {
 }
 
 func (t Training) ActionInfo() (string, error) {
-	distance := spentenergy.Distance(t.Steps, t.Height)
-	speed := spentenergy.MeanSpeed(t.Steps, t.Personal.Height, t.Duration)
-	calories, err := spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
-	if err != nil {
-		return "", err
-	}
 	if t.TrainingType != "Ходьба" && t.TrainingType != "Бег" {
 		return "", errors.New("неизвестный тип тренировки")
+	}
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Personal.Height, t.Duration)
+	var calories float64
+	var err error
+	if t.TrainingType == "Ходьба" {
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	} else {
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	}
+	if err != nil {
+		return "", err
 	}
 	return fmt.Sprintf("Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n", t.TrainingType, t.Duration.Hours(), distance, speed, calories), nil
 }
