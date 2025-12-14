dbdf3f929721365d3704fcd1f8ff325873c05662
Max Dukov <dukov@arrival.com>
2025-12-14 19:00:36 +0300

    first commit

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,6 +1,7 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
@@ -12,18 +13,42 @@ const (
 	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
 )
 
-func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
-}
-
+// RunningSpentCalories рассчитывает количество сожженных калорий при беге
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+	if duration <= 0 || steps <= 0 || weight <= 0 || height <= 0 {
+		return 0, errors.New("неверные данные")
+	}
+
+	speed := meanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+	calories := (weight * speed * durationInMinutes) / minInH
+	return calories, nil
 }
 
+// WalkingSpentCalories рассчитывает количество сожженных калорий при ходьбе
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if duration <= 0 || steps <= 0 || weight <= 0 || height <= 0 {
+		return 0, errors.New("неверные данные")
+	}
+	durationInMinutes := duration.Minutes()
+	speed := meanSpeed(steps, height, duration)
+	calories := walkingCaloriesCoefficient * (weight * speed * durationInMinutes) / minInH
+	return calories, nil
+}
+
+// meanSpeed рассчитывает среднюю скорость, с которой была пройдена дистанция
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+	if steps <= 0 {
+		return 0
+	}
+	return Distance(steps, height) / duration.Hours()
 }
 
+// Distance рассчитывает дистанцию, пройденную за тренировку
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+	stepsLength := float64(steps) * stepLengthCoefficient * height
+	return stepsLength / mInKm
 }
